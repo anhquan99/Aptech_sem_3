d49aa8f8566a929ccc3150c0fe99532292c4a11f
anhquan99 <44637255+anhquan99@users.noreply.github.com>
2020-05-15 19:42:45 +0700

    15//5/2020 commit

---
+++ AptechSem3/AptechSem3/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using AptechSem3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AptechSem3.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DoTest()
+        {
+            List<QuestionAndAnswer> list = new List<QuestionAndAnswer>();
+            List<String> answ = new List<string>();
+            answ.Add("A 1");
+            list.Add(new QuestionAndAnswer(1, "Test Question 1", answ));
+            answ.Add("A 2");
+            list.Add(new QuestionAndAnswer(2, "Test Question 2", answ));
+            answ.Add("A 3");
+            list.Add(new QuestionAndAnswer(3, "Test Question 3", answ));
+            answ.Add("A 4");
+            list.Add(new QuestionAndAnswer(4, "Test Question 4", answ));
+            ViewBag.Question = list;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Submit(List<int> listInt)
+        {
+            Console.WriteLine(listInt + " " +  listString);
+            return View();
+        }
+    }
+}
---
+++ AptechSem3/AptechSem3/Models/QuestionAndAnswer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AptechSem3.Models
+{
+    public class QuestionAndAnswer
+    {
+        public int QuestionId;
+        public String Question;
+        public List<String> Answer;
+        public String Selected;
+        public QuestionAndAnswer(int QuestionId, String Question, List<String> Answer)
+        {
+            this.QuestionId = QuestionId;
+            this.Question = Question;
+            this.Answer = Answer;
+        }
+        public QuestionAndAnswer(int QuestionId)
+        {
+            APTECH_SEM_3Entities db = new APTECH_SEM_3Entities();
+            try
+            {
+                var Question = (from p in db.QUESTIONs where p.QUESTION_ID == QuestionId select p).Single();
+                this.Question = Question.QUESTION1;
+                foreach (var i in Question.ANSWERs)
+                {
+                    this.Answer.Add(i.ANSWER1);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
